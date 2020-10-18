88b504b836de7cac963f72e945ab700a67f32d93
Kentkn192837 <agriculture5012@gmail.com>
2020-10-19 05:06:54 +0900

    魔王と戦闘する簡単なプログラムを作成

--- program.cpp
+++ program.cpp
@@ -2,6 +2,24 @@
 
 int main(void)
 {
-    printf("test");
+    int action;
+    int event = 1;
+
+    printf( "魔王が現れました。\nあなたはどうする？\n" );
+    printf( "1:たたかう  2:にげる\n" );
+    while ( event ) {
+        scanf( "%d" , &action );
+        if ( action == 1 ) {
+            printf( "あなたは魔王と戦った。\n" );
+            break;
+        } else if ( action == 2 ) {
+            printf( "あなたは魔王から逃げ出した。\n" );
+            break;
+        } else {
+            printf( "1か2を選択してください\n" );
+            continue;
+        }
+    }
+
     return 0;
 }
